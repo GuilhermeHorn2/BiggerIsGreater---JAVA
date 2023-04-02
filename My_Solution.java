537d055c4d5976fe6dee7f873e86b8982c25f926
GuilhermeHorn2 <74984552+GuilhermeHorn2@users.noreply.github.com>
2023-04-02 20:35:30 -0300

    Create My_Solution.java

---
+++ My_Solution.java
@@ -0,0 +1,70 @@
+package misc;
+
+public class main_misc2 {
+
+	public static void main(String[] args) {
+		// 
+		String s  = "cdba";
+		String r = big_perm(s);
+		System.out.println(r);
+		//System.out.println(reverse(s));
+		
+		}
+	private static void swap(char[]c,int i,int j) {
+		//
+		char d = c[i];
+		c[i] = c[j];
+		c[j] = d;
+	}
+	/*
+	 * I want to find the first element from right to left and swap when i found a c[i] > c[i-1]
+	 * after that i guarantee that the array from i to v.lenght-1 is sorted from max to min
+	 * so i just have to invert this part of the array.then i have to concatenate both
+	 */
+	private static String reverse(String s) {
+		//
+		char[] c = s.toCharArray();
+		String r = "";
+		for(int i = s.length()-1;i >= 0;i--){
+			//
+			r += c[i];
+		}
+		return r;
+	}
+	private static String big_perm(String s) {
+		char[] c = s.toCharArray();
+		
+		//1st step:find a permutation that is bigger than s and olny cost 1 swap
+		int j = 0;
+		for(int i = c.length-1;i >= 0;i--) {
+			if(i != 0 && c[i] > c[i-1]) {
+				//
+				swap(c,i,i-1);
+				j = i-1;
+				break;
+			}
+		}
+		//2st step:from the swap term to right invert the array
+		String a = "";
+		String b = "";
+		for(int i = 0;i < c.length;i++) {
+			if(i >= j+1) {
+				a += c[i];
+			}
+			if(i < j+1) {
+				b += c[i];
+			}
+		}
+		a = reverse(a);
+		//
+		return b+a;
+		
+	}
+		
+	
+	}
+	
+	
+
+
+
